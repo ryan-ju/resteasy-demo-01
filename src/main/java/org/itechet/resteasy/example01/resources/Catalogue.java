deacfedfa41f933827c4e533729c2352e5c3a34b
ryan-ju <r.ju2014@gmail.com>
2015-12-01 14:22:44 +0000

    Added copyright info

--- src/main/java/org/itechet/resteasy/example01/resources/Catalogue.java
+++ src/main/java/org/itechet/resteasy/example01/resources/Catalogue.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2015.
+ *
+ * This software may be modified and distributed under the terms of the MIT license.
+ * See the LICENSE file for details.
+ */
 package org.itechet.resteasy.example01.resources;
 
 import org.itechet.resteasy.example01.services.CatalogueService;
